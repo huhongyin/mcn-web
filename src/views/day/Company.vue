fb4b00d667c6a73b65e73b5d27e8a6f86668a5b6
Jack <1468018323@qq.com>
2019-11-06 17:57:47 +0800

    d

--- src/views/day/Company.vue
+++ src/views/day/Company.vue
@@ -2,8 +2,13 @@
     <el-card class="box-card" :style="'height:'+ cardHeight +';'">
         <div slot="header" class="clearfix">
             <el-row :gutter="10">
-                <el-col :span="2" style="line-height:40px;text-align:center;">
+                <el-col :span="2" style="line-height:40px;">
                     公司榜
+                </el-col>
+                <el-col :span="4" style="line-height:40px;">
+                    <el-select v-model="search.plat_id">
+                        <el-option v-for="(plat,key) in plats" :key="key" :label="plat.name" :value="plat.id"></el-option>
+                    </el-select>
                 </el-col>
                 <el-col :span="4">
                     <el-date-picker type="date" value-format="yyyy-MM-dd" format="yyyy-MM-dd" v-model="search.date" placeholder="选择查询截止日期" style="max-width:100%;"></el-date-picker>
@@ -11,25 +16,26 @@
                 <el-col :span="3">
                     <el-button type="primary" @click="getData">搜索</el-button>
                 </el-col>
-                <el-col :span="2" :offset="13">
+                <el-col :span="2" :offset="9">
                     <el-button @click="exportExcel" style="float:right;">导出</el-button>
                 </el-col>
             </el-row>
         </div>
-        <div style="width:100%;height:100%;">
+        <div style="width:100%;height:90%;">
             <div :style="'height:' + tableHeight + ';width: 100%'">
-                <pl-table :datas="list" :height-change="true" :pagination-show="false" border id="out-table" v-loading="loading" ref="plTable" header-drag-style use-virtual :row-height="50">
-                    <pl-table-column label="序号" prop="id" fixed></pl-table-column>
-                    <pl-table-column label="主播昵称" prop="nickname" fixed></pl-table-column>
-                    <pl-table-column label="原始ID" prop="plat_actor_id" fixed></pl-table-column>
-                    <pl-table-column label="开播日期" prop="start_time" fixed></pl-table-column>
-                    <pl-table-column label="主播级别" prop="level.name" fixed width="100px;"></pl-table-column>
+                <pl-table :datas="list" :height-change="true" :span-method="objectSpanMethod" :pagination-show="false" border id="out-table" v-loading="loading" ref="plTable" header-drag-style use-virtual :row-height="50">
+                    <pl-table-column label="序号" prop="id"></pl-table-column>
+                    <pl-table-column label="主播昵称" prop="nickname"></pl-table-column>
+                    <pl-table-column label="原始ID" prop="plat_actor_id"></pl-table-column>
+                    <pl-table-column label="开播日期" prop="start_time"></pl-table-column>
+                    <pl-table-column label="主播级别" prop="level.name" width="100px;"></pl-table-column>
                     <pl-table-column label="主播平台" prop="plat.name" width="120px;"></pl-table-column>
+                    <pl-table-column :label="day_title" prop="day_money" width="120px;"></pl-table-column>
                     <pl-table-column label="上月同期收益" prop="last_month_money"></pl-table-column>
                     <pl-table-column label="时长" prop="time" width="100px;"></pl-table-column>
                     <pl-table-column :label="month_title" prop="total_money"></pl-table-column>
                     <pl-table-column label="日均收益" prop="day_avg"></pl-table-column>
-                    <pl-table-column label="累积总收益" prop="all_money"></pl-table-column>
+                    <pl-table-column :label="all_money_title" prop="all_money"></pl-table-column>
                     <pl-table-column label="所属公司" prop="company.name"></pl-table-column>
                 </pl-table>
             </div>
@@ -40,26 +46,32 @@
 <script>
 import FileSaver from 'file-saver'
 import XLSX from 'xlsx'
+import { exportJsonToExcel, formatJson } from 'pl-export-excel'
 import { get } from '@/api/index.js';
 import teamApi from '@/api/team.js';
+import platApi from '@/api/plats.js';
 
 export default {
     data(){
         return {
             tableHeight: '200px',
             cardHeight: 'unset',
+            all_money_title: '',
             month_title: '',
+            day_title: '',
             list: [],
+            plats: [],
             count: 0,
             search: {
                 date: '',
+                plat_id: '',
                 page: 1,
             },
             loading: false,
         }
     },
     created(){
-        this.getData()
+        this.getPlat()
         this.search.date = new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate()
     },
     methods:{
@@ -68,7 +80,9 @@ export default {
             get(teamApi.companyList, this.search).then((res) => {
                 this.loading = false
                 this.list = res.data.list
-                this.month_title = res.data.month + '月总收入'
+                this.month_title = res.data.month
+                this.day_title = res.data.day_title
+                this.all_money_title = res.data.all_money_title
                 if(this.list.length > 0){
                     this.tableHeight = '100%'
                     this.cardHeight = '100%'
@@ -78,43 +92,48 @@ export default {
                 }
             })
         },
+        getPlat(){
+            get(platApi.list).then((res) => {
+                this.plats = res.data.list
+                this.search.plat_id = this.plats[0].id
+                this.getData()
+            })
+        },
         handleCurrentChange(page){
             this.search.page = page
             this.getData()
         },
         objectSpanMethod({ row, column, rowIndex, columnIndex }) {
-            if(columnIndex === 0 || columnIndex === 1){
-                var rowspan = 0
-                var colspan = 0;
-                if(typeof(row.rowspan) != 'undefined'){
-                    rowspan = row.rowspan - 1
-                }
-                if(typeof(row.colspan) != 'undefined'){
-                    colspan = row.colspan
-                }
-                if(row.type == 'total' && columnIndex === 0){
-                    rowspan = 1
-                    return {
-                        rowspan: rowspan,
-                        colspan: colspan,
-                    }
-                }
-                return {
-                    rowspan: rowspan,
-                    colspan: colspan,
-                }
-            }else if(columnIndex > 0 && columnIndex < 12){
-                if(row.type == 'total'){
+            if(row.row_type === 'total'){
+                if(columnIndex == 0){
                     return {
                         rowspan: 0,
                         colspan: 0,
                     }
+                }else if(columnIndex === 1){
+                    var rowspan = 1
+                    var colspan = 1;
+                    if(row.rowspan > 0){
+                        rowspan = row.rowspan
+                    }
+                    if(row.colspan > 0){
+                        colspan = row.colspan
+                    }
+
+                    return {
+                        rowspan: rowspan,
+                        colspan: colspan,
+                    }
+                }else{
+                    return {
+                        colspan: 0,
+                        rowspan: 0,
+                    }
                 }
-            }else{
-                return {
-                    rowspan: 1,
-                    colspan: 1,
-                }
+            }
+            return {
+                rowspan: 1,
+                colspan: 1,
             }
         },
         getSummaries(param) {
@@ -148,33 +167,23 @@ export default {
             return sums;
         },
         exportExcel () {
-            var id = "#out-table"
-            var title = '团队日榜.xlsx'
-            var fix = document.querySelector('.el-table__fixed');
-            var wb;
-            if (fix) {
-                wb = XLSX.utils.table_to_book(document.querySelector(id).removeChild(fix));
-                document.querySelector(id).appendChild(fix);
-            } else {
-                wb = XLSX.utils.table_to_book(document.querySelector(id));
-            }
-            /* get binary string as output */
-            var wbout = XLSX.write(wb, {
-                bookType: 'xlsx',
-                bookSST: true,
-                type: 'array'
-            });
-            try {
-                FileSaver.saveAs(
-                new Blob([wbout], {
-                    type: 'application/octet-stream'
-                }),
-                title
-                );
-            } catch (e) {
-                if (typeof console !== 'undefined') console.log(e, wbout);
-            }
-            return wbout;
+            const tHeader = ['序号', '主播昵称', '原始ID', '开播日期', '主播级别', '主播平台', this.day_title, '上月同期收益', '时长', this.month_title, '日均收益', this.all_money_title, '所属公司']
+            const filterVal = ["id", "nickname", "plat_actor_id", "start_time", "level", "plat.name", "day_money", "last_month_money", "time", "total_money", "day_avg", "all_money", "company.name"]
+            const list = this.list
+            const data = formatJson(filterVal, list)
+            exportJsonToExcel({
+
+                header: tHeader, // 表头
+
+                data: data, // 表格数据
+
+                filename: '公司榜', // 表格名称
+
+                autoWidth: true, // 自适应宽度
+
+                bookType: 'xlsx' // 导出文件的类型
+
+            })
         },
     }
 }
