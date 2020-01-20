7bc9d245c9ae2cf95a0f1a35d8d2ded1b32a623f
柏祥云 <991549215@qq.com>
2020-01-20 17:17:01 +0800

    d

--- src/views/setting/auto_sign.vue
+++ src/views/setting/auto_sign.vue
@@ -32,6 +32,14 @@
             <!-- <el-form-item label="身份证号码">
                 <el-input v-model="form.actor.id_card_no"></el-input>
             </el-form-item> -->
+            <el-form-item label="身份证图片">
+                <el-upload action="https://jsonplaceholder.typicode.com/posts/" list-type="picture-card" :on-preview="handlePictureCardPreview" :on-remove="handleRemove">
+                    <i class="el-icon-plus"></i>
+                </el-upload>
+                <el-dialog :visible.sync="dialogVisible">
+                    <img width="100%" :src="dialogImageUrl" alt="">
+                </el-dialog>
+            </el-form-item>
             <el-form-item label="艺名">
                 <el-input v-model="form.actor_plat.nickname"></el-input>
             </el-form-item>
@@ -145,6 +153,8 @@ export default {
                 "end_month",
                 "end_day",
             ],
+            dialogImageUrl: '',
+            dialogVisible: false,
             form: {
                 fdd: {
                     template_id: "",
@@ -210,6 +220,13 @@ export default {
             this.getLevels()
             this.getGuilds()
             this.getTemplates()
+        },
+        handleRemove(file, fileList) {
+            console.log(file, fileList);
+        },
+        handlePictureCardPreview(file) {
+            this.dialogImageUrl = file.url;
+            this.dialogVisible = true;
         },
         getTemplates(){
             get(autoSignApi.fdd_templates).then((res) => {
